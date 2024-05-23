14ec6242218e9276a85f2c47282a8a52a70999a2
karinatimm <kartimm.webdev@gmail.com>
2024-05-23 08:50:36 +0200

    Added frontend/src/utils/

---
+++ frontend/src/utils/router.js
@@ -0,0 +1,12 @@
+export const ROUTES = {
+  homePage: '/',
+  login: '/login',
+  signup: '/signup',
+};
+
+export const API_ROUTES = {
+  login: '/login',
+  signup: '/signup',
+  channels: '/data',
+  base: '/api/v1',
+};
