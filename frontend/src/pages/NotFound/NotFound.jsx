81348568ac47e2e157e2acd5d6bf29b27c8e2f10
karinatimm <kartimm.webdev@gmail.com>
2024-05-29 14:12:25 +0200

    Updated frontend/src/pages/NotFound/NotFound.jsx

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import notFoundAvatar from '../../assets/imgNotFoundPage/404_error.png';
+import { ROUTES } from '../../utils/router';
 
-const NotFound = () => (
-  <div>
-    <h1>404 Not Found</h1>
-    <p>The page you are looking for does not exist. Try again!</p>
-  </div>
-);
+const NotFoundComponent = () => {
+  const navigate = useNavigate();
 
-export default NotFound;
+  const handleHomePageClick = (event) => {
+    event.preventDefault();
+    navigate(ROUTES.homePage);
+  };
+
+  return (
+    <div className="text-center mt-5">
+      <img src={notFoundAvatar} alt="Page wasn't found" className="img-fluid" />
+      <h1 className="h4 text-muted">Page Not Found</h1>
+      <p className="text-muted">
+        The page you are looking for does not exist. Go back to{' '}
+        <a href={ROUTES.homePage} onClick={handleHomePageClick}>
+          homepage
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default NotFoundComponent;
