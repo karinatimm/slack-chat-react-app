047b6b36ac4263c2bd51187166c2ae7431268da6
karinatimm <kartimm.webdev@gmail.com>
2024-05-29 12:55:39 +0200

    Updated frontend/src/index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
-import AuthProvider from './context/AuthProvider.js';
+import AuthProvider from './context/auth/AuthProvider.js';
+import store from './store/index.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
