28f514bf6c6877e2c47ce7a2680df0995fd36f1a
karinatimm <kartimm.webdev@gmail.com>
2024-05-31 10:33:42 +0200

    Updated frontend/src/pages/Login/Login.jsx with working routes

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -11,15 +11,18 @@ import { loginValidationSchema } from '../../utils/validationSchemas';
 import LoginComponent from '../../components/LoginComponent.jsx';
 import loginAvatar from '../../assets/imgLoginPage/avatar_login.png';
 import { ROUTES } from '../../utils/router';
+import useAuthContext from '../../hooks/useAuthContext.js';
 
 const Login = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [login] = useAuthenticateLogin();
+  const { logIn } = useAuthContext();
 
   const handleFormSubmit = async (values, { setSubmitting, setErrors }) => {
     try {
       const response = await login(values).unwrap();
+      logIn(response.token, values.username);
       dispatch(
         setUserData({ token: response.token, username: values.username })
       );
@@ -41,7 +44,7 @@ const Login = () => {
         {({ isSubmitting, errors }) => (
           <Form className="col-12 col-md-6 mt-3 mt-mb-0">
             <h1 className="text-center mb-4">Войти</h1>
-            <BootstrapForm.Group className="mb-3" controlId="username">
+            <BootstrapForm.Group className="mb-3">
               <BootstrapForm.Label htmlFor="username">
                 Ваш ник
               </BootstrapForm.Label>
@@ -59,7 +62,7 @@ const Login = () => {
               />
             </BootstrapForm.Group>
 
-            <BootstrapForm.Group className="mb-3" controlId="password">
+            <BootstrapForm.Group className="mb-3">
               <BootstrapForm.Label htmlFor="password">
                 Пароль
               </BootstrapForm.Label>
@@ -97,75 +100,3 @@ const Login = () => {
 };
 
 export default Login;
-
-// return (
-//   <LoginComponent loginAvatar={loginAvatar}>
-//     <div className="d-flex justify-content-center align-items-center">
-//       <Container>
-//         <Row className="justify-content-center">
-//           <Col md="4">
-//             <h2 className="text-center">Войти</h2>
-//             <Formik
-//               initialValues={{ username: '', password: '' }}
-//               validationSchema={loginValidationSchema}
-//               onSubmit={handleFormSubmit}
-//             >
-//               {({ isSubmitting, errors }) => (
-//                 <Form>
-//                   <BootstrapForm.Group className="mb-3" controlId="username">
-//                     <BootstrapForm.Label htmlFor="username">
-//                       Ваш ник
-//                     </BootstrapForm.Label>
-//                     <Field
-//                       name="username"
-//                       as={BootstrapForm.Control}
-//                       type="text"
-//                       id="username"
-//                       placeholder="Введите имя пользователя"
-//                     />
-//                     <ErrorMessage
-//                       name="username"
-//                       component="div"
-//                       className="text-danger"
-//                     />
-//                   </BootstrapForm.Group>
-
-//                   <BootstrapForm.Group className="mb-3" controlId="password">
-//                     <BootstrapForm.Label htmlFor="password">
-//                       Пароль
-//                     </BootstrapForm.Label>
-//                     <Field
-//                       name="password"
-//                       as={BootstrapForm.Control}
-//                       type="password"
-//                       id="password"
-//                       placeholder="Введите имя пользователя"
-//                     />
-//                     <ErrorMessage
-//                       name="password"
-//                       component="div"
-//                       className="text-danger"
-//                     />
-//                   </BootstrapForm.Group>
-
-//                   {errors.general && (
-//                     <div className="text-danger mb-3">{errors.general}</div>
-//                   )}
-
-//                   <Button
-//                     type="submit"
-//                     variant="primary"
-//                     disabled={isSubmitting}
-//                     block
-//                   >
-//                     Войти
-//                   </Button>
-//                 </Form>
-//               )}
-//             </Formik>
-//           </Col>
-//         </Row>
-//       </Container>
-//     </div>
-//   </LoginComponent>
-// );
