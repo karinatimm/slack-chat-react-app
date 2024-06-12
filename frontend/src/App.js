d951f016840395de99c169b0c7c861f40b5c0015
karinatimm <kartimm.webdev@gmail.com>
2024-06-12 11:50:03 +0200

    Updated frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ROUTES } from './utils/router';
-import Login from './pages/Login/Login.jsx';
-import SignUp from './pages/SignUp/SignUp.jsx';
-import NotFound from './pages/NotFound/NotFound.jsx';
-import Home from './pages/Home/Home.jsx';
+import Login from './pages/Login/LogInPageComponent.jsx';
+import SignUp from './pages/SignUp/SignUpPageComponent.jsx';
+import NotFound from './pages/NotFound/NotFoundPageComponent.jsx';
+import Home from './pages/Home/HomePageComponent.jsx';
 import PrivateRoute from './containers/Routes/PrivateRoute.jsx';
+import MainNav from './components/MainNav.jsx';
 
 const getElement = (key) => {
   switch (key) {
@@ -26,12 +27,15 @@ const getElement = (key) => {
 
 const App = () => (
   <BrowserRouter>
-    <Routes>
-      {Object.entries(ROUTES).map(([key, path]) => (
-        <Route key={key} path={path} element={getElement(key)} />
-      ))}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div className="d-flex flex-column h-100">
+      <MainNav />
+      <Routes>
+        {Object.entries(ROUTES).map(([key, path]) => (
+          <Route key={key} path={path} element={getElement(key)} />
+        ))}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   </BrowserRouter>
 );
 
