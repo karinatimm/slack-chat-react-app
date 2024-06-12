8f347b1e5aa21848cbc98e00f58dcb95afba5443
karinatimm <kartimm.webdev@gmail.com>
2024-06-12 11:50:20 +0200

    Updated frontend/src/api/authApi.js

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,22 +1,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import useAuthHeaders from '../hooks/useAuthHeaders';
+// import useAuthHeaders from '../hooks/useAuthHeaders';
 import { API_ROUTES } from '../utils/router';
 
 export const authenticateApi = createApi({
   reducerPath: 'authentication',
   baseQuery: fetchBaseQuery({
     baseUrl: API_ROUTES.base,
-    prepareHeaders: useAuthHeaders,
+    // prepareHeaders: useAuthHeaders,
   }),
   endpoints: (builder) => ({
-    login: builder.mutation({
+    logInRequest: builder.mutation({
       query: (existingUser) => ({
         url: API_ROUTES.login,
         method: 'POST',
         body: existingUser,
       }),
     }),
-    signUp: builder.mutation({
+    signUpRequest: builder.mutation({
       query: (newUser) => ({
         url: API_ROUTES.signup,
         method: 'POST',
@@ -27,6 +27,6 @@ export const authenticateApi = createApi({
 });
 
 export const {
-  useLoginMutation: useAuthenticateLogin,
-  useSignUpMutation: useAuthenticateSignUp,
+  useLogInRequestMutation: useAuthenticateLogin,
+  useSignUpRequestMutation: useAuthenticateSignUp,
 } = authenticateApi;
