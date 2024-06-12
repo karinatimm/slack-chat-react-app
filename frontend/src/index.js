5a8ca90cd4eaecd65675c153d4f3e519a0740491
karinatimm <kartimm.webdev@gmail.com>
2024-06-12 11:52:07 +0200

    Updated frontend/src/index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
-import AuthProvider from './context/auth/AuthProvider.js';
-import store from './store/index.js';
+import Init from './init';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </Provider>
-  </React.StrictMode>
-);
+const renderApp = async () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  const app = await Init();
+  root.render(<React.StrictMode>{app}</React.StrictMode>);
+};
+
+renderApp();
