aba084c66dfbdefb32399e72981f899b95f91e83
karinatimm <kartimm.webdev@gmail.com>
2024-06-19 11:43:54 +0200

    Added src/api/channelsApi.js

---
+++ frontend/src/api/channelsApi.js
@@ -0,0 +1,47 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import useCustomHeaders from '../hooks/useCustomHeaders';
+import { API_ROUTES } from '../utils/router';
+
+export const channelsApi = createApi({
+  reducerPath: 'channelsApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: API_ROUTES.baseChannels,
+    prepareHeaders: useCustomHeaders,
+  }),
+  tagTypes: ['Channel'],
+  endpoints: (builder) => ({
+    fetchChannels: builder.query({
+      query: () => '',
+      providesTags: ['Channel'],
+    }),
+    createChannel: builder.mutation({
+      query: (newChannel) => ({
+        method: 'POST',
+        body: newChannel,
+      }),
+      invalidatesTags: ['Channel'],
+    }),
+    updateChannel: builder.mutation({
+      query: ({ id, ...patch }) => ({
+        url: id,
+        method: 'PATCH',
+        body: patch,
+      }),
+      invalidatesTags: ['Channel'],
+    }),
+    deleteChannel: builder.mutation({
+      query: ({ id }) => ({
+        url: id,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Channel'],
+    }),
+  }),
+});
+
+export const {
+  useFetchChannelsQuery,
+  useCreateChannelMutation,
+  useUpdateChannelMutation,
+  useDeleteChannelMutation,
+} = channelsApi;
