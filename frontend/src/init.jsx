66753102b26e62fb765933b90bd5eeb99c572d0f
karinatimm <kartimm.webdev@gmail.com>
2024-06-19 11:40:11 +0200

    Updated src/init.jsx

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import { I18nextProvider } from 'react-i18next';
+import { io } from 'socket.io-client';
 import i18next from 'i18next';
 import filter from 'leo-profanity';
 import App from './App';
-import AuthProvider from './context/auth/AuthProvider';
-import store from './store';
-import resources from './locales';
+import AuthProvider from './context/authentication/AuthProvider';
+import SocketProvider from './context/socketConnection/SocketProvider.jsx';
+import store from './store/store';
+import resources from './locales/index.js';
 import DEFAULT_LANGUAGE from './utils/config';
 
 const Init = async () => {
+  const socket = io();
   const i18n = i18next.createInstance();
 
   await i18n.init({
@@ -24,16 +28,29 @@ const Init = async () => {
   filter.loadDictionary('ru');
   filter.loadDictionary('en');
 
+  const rollbarConfig = {
+    accessToken: process.env.REACT_APP_ROLLBAR_ACCESS_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    environment: 'production',
+  };
+
   return (
-    <Provider store={store}>
-      <React.StrictMode>
-        <I18nextProvider i18n={i18n}>
-          <AuthProvider>
-            <App />
-          </AuthProvider>
-        </I18nextProvider>
-      </React.StrictMode>
-    </Provider>
+    <React.StrictMode>
+      <RollbarProvider config={rollbarConfig}>
+        <ErrorBoundary>
+          <Provider store={store}>
+            <I18nextProvider i18n={i18n}>
+              <SocketProvider socket={socket}>
+                <AuthProvider>
+                  <App />
+                </AuthProvider>
+              </SocketProvider>
+            </I18nextProvider>
+          </Provider>
+        </ErrorBoundary>
+      </RollbarProvider>
+    </React.StrictMode>
   );
 };
 
