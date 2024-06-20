2a583fb29bf2ce7923100ed8f81aef1b049a09b8
karinatimm <kartimm.webdev@gmail.com>
2024-06-20 16:28:06 +0200

    Updated src/index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Init from './init';
 
 const renderApp = async () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
   const app = await Init();
-  root.render(<React.StrictMode>{app}</React.StrictMode>);
+  root.render(app);
 };
 
 renderApp();
