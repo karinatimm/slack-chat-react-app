62845d02bfbfd2bb9d9468cbbc7c700c62143c8d
karinatimm <kartimm.webdev@gmail.com>
2024-06-20 16:28:27 +0200

    Updated src/init.jsx

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -3,37 +3,19 @@ import { Provider } from 'react-redux';
 import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import { I18nextProvider } from 'react-i18next';
 import { io } from 'socket.io-client';
-import i18next from 'i18next';
-import filter from 'leo-profanity';
 import App from './App';
 import AuthProvider from './context/authentication/AuthProvider';
 import SocketProvider from './context/socketConnection/SocketProvider.jsx';
 import store from './store/store';
-import resources from './locales/index.js';
-import DEFAULT_LANGUAGE from './utils/config';
+import initializeI18n from './utils/i18nConfig';
+import initializeFilter from './utils/filterConfig';
+import rollbarConfig from './utils/rollbarConfig';
 
 const Init = async () => {
   const socket = io();
-  const i18n = i18next.createInstance();
+  const i18n = await initializeI18n();
 
-  await i18n.init({
-    lng: DEFAULT_LANGUAGE,
-    debug: false,
-    resources,
-    interpolation: {
-      escapeValue: false,
-    },
-  });
-
-  filter.loadDictionary('ru');
-  filter.loadDictionary('en');
-
-  const rollbarConfig = {
-    accessToken: process.env.REACT_APP_ROLLBAR_ACCESS_TOKEN,
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-    environment: 'production',
-  };
+  initializeFilter();
 
   return (
     <React.StrictMode>
