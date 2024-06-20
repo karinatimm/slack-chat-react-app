934b67364af84764a1d37189ec637b2ea21fdc0c
karinatimm <kartimm.webdev@gmail.com>
2024-06-20 16:30:37 +0200

    Updated src/utils/config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,16 @@
-const DEFAULT_LANGUAGE = 'ru';
+export const DEFAULT_LANGUAGE = 'ru';
 
-export default DEFAULT_LANGUAGE;
+export const DEFAULT_AUTH_CONTEXT_VALUE = {
+  isAuthenticated: false,
+  logIn: () => {},
+  logOut: () => {},
+};
+
+export const DEFAULT_SOCKET_CONTEXT_VALUE = {
+  socket: {},
+};
+
+export const DEFAULT_CHANNEL = {
+  name: 'general',
+  id: '1',
+};
