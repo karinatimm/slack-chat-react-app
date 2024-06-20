29477b5983b0fbc5fe66c004debf577c3d20cff7
karinatimm <kartimm.webdev@gmail.com>
2024-06-20 16:32:06 +0200

    Added src/containers/ModalContainers/

---
+++ frontend/src/containers/ModalContainers/DeletingChannelModal.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import { Modal, Button, FormGroup } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
+import { switchChannel } from '../../store/entities/appSlice.js';
+import { useDeleteChannelMutation } from '../../api/channelsApi.js';
+
+const DeleteChannelModalComponent = ({ handleClosingModalWindow }) => {
+  const { currentChannelId, currEditedChannelId } = useSelector(
+    (state) => state.appManaging,
+  );
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const [deleteChannel] = useDeleteChannelMutation();
+
+  const handleDeletingChannel = async () => {
+    try {
+      await deleteChannel({ id: currEditedChannelId });
+
+      if (currentChannelId === currEditedChannelId) {
+        dispatch(switchChannel({ name: 'general', id: '1' }));
+      }
+
+      toast.success(t('homePage.modalWindow.channelRemoved'), {
+        position: 'top-center',
+        autoClose: 2000,
+      });
+
+      handleClosingModalWindow();
+    } catch (err) {
+      toast.error(t('homePage.modalWindow.channelRemoveError'), {
+        position: 'top-center',
+        autoClose: 2000,
+      });
+    }
+  };
+
+  return (
+    <Modal show centered onHide={handleClosingModalWindow}>
+      <Modal.Header closeButton>
+        <Modal.Title>{t('homePage.modalWindow.deleteChannel')}</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <Formik
+          initialValues={{}}
+          onSubmit={async () => {
+            await handleDeletingChannel();
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <p className="lead">
+                {t('homePage.modalWindow.confirmationOfChannelDeletion')}
+              </p>
+              <FormGroup className="d-flex justify-content-end">
+                <Button
+                  variant="secondary"
+                  type="button"
+                  className="me-2"
+                  onClick={handleClosingModalWindow}
+                >
+                  {t('homePage.modalWindow.button.resetButton')}
+                </Button>
+                <Button variant="danger" type="submit" disabled={isSubmitting}>
+                  {t('homePage.modalWindow.button.sendButton')}
+                </Button>
+              </FormGroup>
+            </Form>
+          )}
+        </Formik>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default DeleteChannelModalComponent;
---
+++ frontend/src/containers/ModalContainers/RenamingChannelModal.jsx
@@ -0,0 +1,117 @@
+import React, { useEffect, useRef } from 'react';
+import {
+  Formik, Form, Field, ErrorMessage,
+} from 'formik';
+import { Modal, Form as BootstrapForm, Button } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
+import filter from 'leo-profanity';
+import { useUpdateChannelMutation } from '../../api/channelsApi.js';
+import { useChannelValidationSchema } from '../../utils/validationSchemas.js';
+
+const RenameChannelModalComponent = ({ handleClosingModalWindow }) => {
+  const { currEditedChannelId, currEditedChannel } = useSelector(
+    (state) => state.appManaging,
+  );
+  const { t } = useTranslation();
+  const channelSchema = useChannelValidationSchema();
+  const [updateChannel] = useUpdateChannelMutation();
+  const modalInputRef = useRef();
+
+  useEffect(() => {
+    modalInputRef.current.focus();
+    modalInputRef.current.select();
+  }, []);
+
+  const handleRenamingChannel = async (channelName) => {
+    const cleanedChannelName = filter.clean(channelName);
+
+    const newChannel = {
+      id: currEditedChannelId,
+      name: cleanedChannelName,
+    };
+
+    try {
+      await updateChannel(newChannel);
+      toast.success(t('homePage.modalWindow.channelRenamed'), {
+        position: 'top-center',
+        autoClose: 2000,
+      });
+      handleClosingModalWindow();
+    } catch (error) {
+      toast.error(t('homePage.modalWindow.channelRenameError'), {
+        position: 'top-center',
+        autoClose: 2000,
+      });
+    }
+  };
+
+  return (
+    <Modal show centered onHide={handleClosingModalWindow}>
+      <Modal.Header closeButton>
+        <Modal.Title>{t('homePage.modalWindow.renameChannel')}</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <Formik
+          initialValues={{ name: currEditedChannel }}
+          validationSchema={channelSchema}
+          onSubmit={(values, { setSubmitting }) => {
+            handleRenamingChannel(values.name);
+            setSubmitting(false);
+          }}
+          validateOnBlur={false}
+          validateOnChange={false}
+        >
+          {({
+            isSubmitting, handleChange, handleBlur, values,
+          }) => (
+            <Form>
+              <BootstrapForm.Group>
+                <BootstrapForm.Label htmlFor="name">
+                  {t('homePage.modalWindow.addedChannelName')}
+                </BootstrapForm.Label>
+                <Field name="name">
+                  {({ field }) => (
+                    <BootstrapForm.Control
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.name}
+                      name={field.name}
+                      type="text"
+                      id="name"
+                      placeholder={t('homePage.modalWindow.addedChannelName')}
+                      ref={modalInputRef}
+                    />
+                  )}
+                </Field>
+                <ErrorMessage
+                  name="name"
+                  component="div"
+                  className="text-danger"
+                />
+              </BootstrapForm.Group>
+
+              <BootstrapForm.Group className="d-flex justify-content-end mt-3">
+                <Button
+                  variant="secondary"
+                  type="button"
+                  className="me-2"
+                  onClick={handleClosingModalWindow}
+                >
+                  {t('homePage.modalWindow.button.resetButton')}
+                </Button>
+                <Button variant="primary" type="submit" disabled={isSubmitting}>
+                  {t('homePage.modalWindow.button.sendButton')}
+                </Button>
+              </BootstrapForm.Group>
+            </Form>
+          )}
+        </Formik>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default RenameChannelModalComponent;
