4951e42f2aeb6f59b2bc5e5e9016842391bb3ee0
karinatimm <kartimm.webdev@gmail.com>
2024-06-21 14:33:26 +0200

    Added src/context/toast/

---
+++ frontend/src/context/toast/ToastProvider.js
@@ -0,0 +1,35 @@
+import React, { useMemo, useCallback } from 'react';
+import { toast as showToast } from 'react-toastify';
+import ToastContext from './ToastContext';
+
+const ToastProvider = ({ children }) => {
+  const toastOptions = useMemo(
+    () => ({
+      position: 'top-center',
+      autoClose: 2000,
+    }),
+    [],
+  );
+
+  const showToastMessage = useCallback(
+    (message, options = {}) => {
+      showToast(message, { ...toastOptions, ...options });
+    },
+    [toastOptions],
+  );
+
+  const contextValue = useMemo(
+    () => ({
+      showToastMessage,
+    }),
+    [showToastMessage],
+  );
+
+  return (
+    <ToastContext.Provider value={contextValue}>
+      {children}
+    </ToastContext.Provider>
+  );
+};
+
+export default ToastProvider;
