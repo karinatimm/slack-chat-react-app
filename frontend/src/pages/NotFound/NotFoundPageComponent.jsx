310acd01dca2b2ab441feb77477716113235db7a
karinatimm <kartimm.webdev@gmail.com>
2024-06-21 14:37:48 +0200

    Updated src/pages/NotFound/NotFoundPageComponent.jsx

--- frontend/src/pages/NotFound/NotFoundPageComponent.jsx
+++ frontend/src/pages/NotFound/NotFoundPageComponent.jsx
@@ -1,28 +1,23 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import notFoundAvatar from '../../assets/imgNotFoundPage/404_error.png';
 import { ROUTES } from '../../utils/router';
 
 const NotFoundComponent = () => {
   const { t } = useTranslation();
-  const navigate = useNavigate();
-
-  const handleHomePageClick = (event) => {
-    event.preventDefault();
-    navigate(ROUTES.homePage);
-  };
 
   return (
     <div className="text-center mt-5">
-      <img src={notFoundAvatar} alt="Page wasn't found" className="img-fluid" />
+      <img
+        src={notFoundAvatar}
+        alt={t('notFoundPage.img.notFoundAlt')}
+        className="img-fluid"
+      />
       <h1 className="h4 text-muted">{t('notFoundPage.notFoundPageTitle')}</h1>
       <p className="text-muted">
         {t('notFoundPage.notFoundPageText')}
         {' '}
-        <a href={ROUTES.homePage} onClick={handleHomePageClick}>
-          {t('notFoundPage.notFoundPageLink')}
-        </a>
+        <Link to={ROUTES.homePage}>{t('notFoundPage.notFoundPageLink')}</Link>
       </p>
     </div>
   );
