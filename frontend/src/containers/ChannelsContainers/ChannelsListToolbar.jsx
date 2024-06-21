87128a16253e90dcfd6a0c66ad1b7787c2347d3c
karinatimm <kartimm.webdev@gmail.com>
2024-06-21 14:34:14 +0200

    Updated src/containers/ChannelsContainers/ChannelsListToolbar.jsx

--- frontend/src/containers/ChannelsContainers/ChannelsListToolbar.jsx
+++ frontend/src/containers/ChannelsContainers/ChannelsListToolbar.jsx
@@ -23,7 +23,10 @@ const ChannelsListToolbar = ({ channels }) => {
   };
 
   return (
-    <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex ">
+    <div
+      className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex"
+      style={{ minHeight: '100vh' }}
+    >
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
         <b>{t('homePage.channels')}</b>
         <button
@@ -43,6 +46,7 @@ const ChannelsListToolbar = ({ channels }) => {
       <ul
         id="channels-box"
         className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block"
+        style={{ minHeight: 'calc(100vh - 100px)' }}
       >
         {channels?.map((channel) => (
           <li key={channel.id} className="nav-item w-100">
