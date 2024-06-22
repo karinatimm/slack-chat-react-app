4850554a1ef40c9bd0a4f92b1d1e337bd5670eac
karinatimm <kartimm.webdev@gmail.com>
2024-06-22 11:39:35 +0200

    Updated add src/components/PagesComponents/LogIn.jsx

--- frontend/src/components/PagesComponents/LogIn.jsx
+++ frontend/src/components/PagesComponents/LogIn.jsx
@@ -7,10 +7,7 @@ const LoginComponent = ({ children, loginAvatar }) => {
 
   return (
     <div className="container-fluid h-100">
-      <div
-        className="row justify-content-center align-content-center h-100"
-        style={{ minHeight: '100vh' }}
-      >
+      <div className="row justify-content-center align-content-center h-100">
         <div className="col-12 col-md-8 col-xxl-6">
           <div className="card shadow-sm">
             <div className="card-body row p-5">
