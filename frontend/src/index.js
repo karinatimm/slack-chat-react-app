c8be2fce392550b4c5bab1bda80c7163c9645097
karinatimm <kartimm.webdev@gmail.com>
2024-06-22 11:45:08 +0200

    Updated src/index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import init from './init';
+import './style.css';
 
 const renderApp = async () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
