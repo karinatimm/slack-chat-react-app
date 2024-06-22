540cb0fc7f67d5434d6c4105a340e41f1eb31700
karinatimm <kartimm.webdev@gmail.com>
2024-06-22 11:39:54 +0200

    Updated src/components/PagesComponents/SignUp.jsx

--- frontend/src/components/PagesComponents/SignUp.jsx
+++ frontend/src/components/PagesComponents/SignUp.jsx
@@ -5,10 +5,7 @@ const SignupComponent = ({ children, signUpAvatar }) => {
 
   return (
     <div className="container-fluid h-100">
-      <div
-        className="row justify-content-center align-content-center h-100"
-        style={{ minHeight: '100vh' }}
-      >
+      <div className="row justify-content-center align-content-center h-100">
         <div className="col-12 col-md-8 col-xxl-6">
           <div className="card shadow-sm">
             <div className="card-body d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
