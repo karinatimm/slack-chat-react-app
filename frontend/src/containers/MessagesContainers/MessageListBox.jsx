fa853ff6f9f5c5c5e4a0da539909e9ebbf35b102
karinatimm <kartimm.webdev@gmail.com>
2024-06-22 11:41:43 +0200

    Updated src/containers/MessagesContainers/MessageListBox.jsx

--- frontend/src/containers/MessagesContainers/MessageListBox.jsx
+++ frontend/src/containers/MessagesContainers/MessageListBox.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import extractCurrentChannelMessages from '../../utils/messageUtils';
@@ -16,8 +15,8 @@ const MessageListBox = ({ messages, children }) => {
   const numOfMessages = currentChannelMessages.length;
 
   return (
-    <div className="col p-0 h-100" style={{ minHeight: '100vh' }}>
-      <div className="d-flex flex-column h-100" style={{ minHeight: '100vh' }}>
+    <div className="col p-0 h-100">
+      <div className="d-flex flex-column h-100">
         <div className="bg-light mb-4 p-3 shadow-sm small">
           <p className="m-0">
             <b>{currentChannel?.name}</b>
