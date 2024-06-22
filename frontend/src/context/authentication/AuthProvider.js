13a8c4c7d1dbad8ca1dd71b1193663640a974a55
karinatimm <kartimm.webdev@gmail.com>
2024-06-22 11:43:06 +0200

    Updated src/context/authentication/AuthProvider.js

--- frontend/src/context/authentication/AuthProvider.js
+++ frontend/src/context/authentication/AuthProvider.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { AuthContext } from './AuthContext.js';
 
 const AuthProvider = ({ children }) => {
